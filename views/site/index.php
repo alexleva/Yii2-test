a6252a5dc14e1800ab54e119906cd1eb79610ac8
alexleva <36234185+alexleva@users.noreply.github.com>
2018-03-05 14:40:28 +0200

    1
    1

--- views/site/index.php
+++ views/site/index.php
@@ -16,14 +16,14 @@ $this->title = 'My Yii Application';
         <div class="row">
             <?php foreach ($categories as $category): ?>
             <div class="col-lg-4">
-                <?php if($category[type] == 'dinamic'): ?>
-                <h2 class="text-success"><?= $category->name ?></h2>
+                <?php if($category->type == 'dinamic'): ?>
+                <h2 class="text-success"><?php echo $category->name ?></h2>
                     <?php endif;?>
-                <?php if($category[type] == 'static'): ?>
-                    <h2 class="text-danger"><?= $category->name ?></h2>
+                <?php if($category->type == 'static'): ?>
+                    <h2 class="text-danger"><?php echo $category->name ?></h2>
                 <?php endif;?>
 
-                <p><a class="btn btn-default" href="<?=Url::to([$category[name]]);  ?>">Перейти к категории &raquo;</a></p>
+                <p><a class="btn btn-default" href="<?php echo Url::to([$category->name]);  ?>">Перейти к категории &raquo;</a></p>
 
             </div>
             <?endforeach; ?>
